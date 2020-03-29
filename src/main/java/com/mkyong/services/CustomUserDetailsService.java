87ec9ac123754d47c63c2d06443170b2dfc5d719
STEIGERWALD <47118413+Steigerwald@users.noreply.github.com>
2020-03-29 18:51:47 +0200

    Rajout des entités Role et User pour spring sécurity

---
+++ src/main/java/com/mkyong/services/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.mkyong.services;
+
+
+import com.mkyong.entity.User;
+import com.mkyong.repository.UserRepository;
+import org.hibernate.mapping.Collection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(userName)
+                .orElseThrow(() -> new UsernameNotFoundException("Email " + userName + " not found"));
+        return new org.springframework.security.core.userdetails.User(user.getMailUser(), user.getMotDePasseUser(),
+                getAuthorities(user));
+    }
+
+    private static Collection<? extends GrantedAuthority> getAuthorities(User user) {
+        String[] userRoles = user.getRoles().stream().map((role) -> role.getNomRole()).toArray(String[]::new);
+        Collection<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList(userRoles);
+        return authorities;
+    }
+}
+
