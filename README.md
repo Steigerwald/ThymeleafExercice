a73077d3eff937606c329364bad19de3f5dc27a1
STEIGERWALD <47118413+Steigerwald@users.noreply.github.com>
2020-07-05 19:42:58 +0200

    correction readme et mot de passe pour user et admin

--- README.md
+++ README.md
@@ -1,31 +1,32 @@
-# Thymeleafexercice
+# OpenClassRoom projet 6
 
 Projet de conception d'un site de partage d'information sur l'escalade
 
-## 1. How to start
+## 1. Comment lancer l'application:
 ```
-
-git clone [https://github.com/Steigerwald/ThymeleafExercice.git]
+git clone https://github.com/Steigerwald/ThymeleafExercice.git
 cd web-thymeleaf
 mvn spring-boot:run
-
 ```
-# ThymeleafExercice
-
-# comment lancer la base de données
-
-pour lancer le projet, vous devez d'abord créer la base de données avec le nom escalades
-avec les paramètres suivants:
-
-service:Mysql@localhost:3306
-user:root
-
-MP:Admin!1973
+## 2. Comment lancer la base de données
+L'application nécessite une base de données MySQL avec les informations suivantes:
+- nom de la base de données: escalades
+- port: 3306
+- user: root
+- password: Admin!1973
 
 lancer ensuite l'application pour qu'elle crée les tables gràce aux entités
 
+## 3. Remplir la base de données avec un jeu de données Test
 
-# les scripts
-le fichier des données :
-data.SQL à charger à partir de MySQLWorkbench pour enregistrer les données dans la base
+exécuter le fichier data.sql situé dans le dossier ressources.
 
+informations de connection:
+
+role admin: 
+- identifiant: admin@gmail.com 
+- password: coco
+
+role user: 
+- identifiant: user@gmail.com
+- password: coco
--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -16,11 +16,11 @@ INSERT INTO escalades.TBL_ROLE (id_role, nom_role) VALUES
 (2, 'ROLE_USER');
 
 INSERT INTO escalades.TBL_USER (id_user, mail_user, mot_de_passe_user, nom_user,prenom_user,role_id_role) VALUES
-(1, 'admin@gmail.com', '$2a$10$2Mgmm6OlYvb54/SQik3EO.jCHE4RA4S5lpDSjmD73sDnS1zhPK1Ru', 'Steigerwald','Brice',1),
-(2, 'user@gmail.com', '$2a$10$2Mgmm6OlYvb54/SQik3EO.jCHE4RA4S5lpDSjmD73sDnS1zhPK1Ru', 'Steigerwald','Jacques',2),
-(3, 'tara@gmail.com', '$2a$10$2Mgmm6OlYvb54/SQik3EO.jCHE4RA4S5lpDSjmD73sDnS1zhPK1Ru', 'Hot','Tara',2),
-(4, 'jean@gmail.com', '$2a$10$2Mgmm6OlYvb54/SQik3EO.jCHE4RA4S5lpDSjmD73sDnS1zhPK1Ru', 'Pierre','Jean',2),
-(5, 'anne@gmail.com', '$2a$10$2Mgmm6OlYvb54/SQik3EO.jCHE4RA4S5lpDSjmD73sDnS1zhPK1Ru', 'Priska','Anne',2);
+(1, 'admin@gmail.com', '$2a$10$GQXfIS9n2oJuE1lVI70yFeu5iJn/PEm5B0CUrfGEUT7bSRcsuqCnS', 'Steigerwald','Brice',1),
+(2, 'user@gmail.com', '$2a$10$GQXfIS9n2oJuE1lVI70yFeu5iJn/PEm5B0CUrfGEUT7bSRcsuqCnS', 'Steigerwald','Jacques',2),
+(3, 'tara@gmail.com', '$2a$10$GQXfIS9n2oJuE1lVI70yFeu5iJn/PEm5B0CUrfGEUT7bSRcsuqCnS', 'Hot','Tara',2),
+(4, 'jean@gmail.com', '$2a$10$GQXfIS9n2oJuE1lVI70yFeu5iJn/PEm5B0CUrfGEUT7bSRcsuqCnS', 'Pierre','Jean',2),
+(5, 'anne@gmail.com', '$2a$10$GQXfIS9n2oJuE1lVI70yFeu5iJn/PEm5B0CUrfGEUT7bSRcsuqCnS', 'Priska','Anne',2);
 
 INSERT INTO escalades.TBL_VOIE (id_voie, cotation, nombre_longueurs, nombre_points,numero_voie,secteur_id_secteur) VALUES
 (1, '2c', 2, 3,'1',1),
@@ -53,15 +53,15 @@ INSERT INTO escalades.TBL_SITE (id_site, descriptif, lieu, nom_site,officiel,top
 (6, 'site de toutes les peurs', 'Hymalaya français','le mont trop dur',1,5,0,5);
 
 INSERT INTO escalades.TBL_TOPO (id_topo, date_parution, description, disponible, location, nom_topo,owner_id_user) VALUES
-(1,'2019-09-28 02:45:30', 'topo pour profiter de l"ile, de la montagne et des volcans',0,1,'La balade complète',4),
-(2,'2019-11-28 02:55:30', 'topo pour profiter du centre montagneux de la France',0,1,'La vieille montagne',4),
-(3,'2020-01-28 11:45:30', 'topo pour profiter de la Bretagne, des collines de granite',0,1,'Kenavo',5),
-(4,'2020-02-14 11:45:30', 'topo pour profiter de la haute altitude',0,1,'Alpes',1),
-(5,'2020-04-14 11:45:30', 'topo pour profiter des plaines',0,1,'Corèze',3);
+(1,'2019-09-28 02:45:30', 'topo pour profiter de l"ile, de la montagne et des volcans','reservé',1,'La balade complète',4),
+(2,'2019-11-28 02:55:30', 'topo pour profiter du centre montagneux de la France','reservé',1,'La vieille montagne',4),
+(3,'2020-01-28 11:45:30', 'topo pour profiter de la Bretagne, des collines de granite','reservé',1,'Kenavo',5),
+(4,'2020-02-14 11:45:30', 'topo pour profiter de la haute altitude','reservé',1,'Alpes',1),
+(5,'2020-04-14 11:45:30', 'topo pour profiter des plaines','reservé',1,'Corèze',3);
 
 INSERT INTO escalades.TBL_RESERVATION (id_reservation, etat, date_reservation,topo_id_topo,user_id_user) VALUES
 (1,'acceptee','2020-04-28 02:45:30',2,1),
-(2,'en attente','2020-04-21 02:45:30',3,2),
+(2,'attente','2020-04-21 02:45:30',3,2),
 (3,'acceptee','2020-05-04 02:45:30',1,1),
 (4,'refusee','2020-05-25 02:45:30',4,2);
 
